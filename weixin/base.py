17103bc4baf6acd8c685b6c26a873d6924bf4a58
catroll <ninedoors@126.com>
2021-04-28 11:36:38 +0800

    放弃 python2 的支持

--- weixin/base.py
+++ weixin/base.py
@@ -4,16 +4,6 @@
 __all__ = ('WechatError',)
 
 
-try:
-    unicode = unicode
-except NameError:
-    # python 3
-    basestring = (str, bytes)
-else:
-    # python 2
-    bytes = str
-
-
 class WechatError(Exception):
 
     def __init__(self, msg):
