283e22320f3e1b3a0dc332f90332589c63c4b635
catroll <ninedoors@126.com>
2021-04-29 11:45:25 +0800

    example: merge: views/share.py views/mp.py

--- example/views/__init__.py
+++ example/views/__init__.py
@@ -1,5 +1,4 @@
 from .auth import app as bp_auth  # noqa
 from .msg import app as bp_msg  # noqa
 from .pay import app as bp_pay  # noqa
-from .share import app as bp_share  # noqa
 from .mp import app as bp_mp  # noqa
--- example/views/mp.py
+++ example/views/mp.py
@@ -3,14 +3,36 @@ from flask import (Blueprint, Response, current_app, redirect, render_template,
 from wechat.base import random_string
 
 app = Blueprint('mp', __name__)
-SCENE_PREFIX = 'mp-login-'
+
+
+@app.route('/wechat.share.action')
+def view_wechat_share():
+    """
+    1. access_token
+    2. jsapi_ticket
+    3. jsapi_ticket + noncestr + timestamp + url => signature
+    """
+    sign = current_app.wechat.mp.jsapi_sign(url=request.url)
+    current_app.logger.debug('jsapi sign: %r', sign)
+    data = {
+        # 开启调试模式，调用的所有api的返回值会在客户端alert出来，
+        # 若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        'debug': True,
+        'appId': sign['appId'],
+        'timestamp': sign['timestamp'],
+        'nonceStr': sign['noncestr'],
+        'signature': sign['sign'],
+        'jsApiList': ['updateAppMessageShareData', 'updateTimelineShareData'],
+    }
+    current_app.logger.debug('wx config: %r', data)
+    return render_template('web_wechat_share.html', bridge_params=json.dumps(data))
 
 
 @app.route('/wechat.mp.follow.action')
 def view_wechat_mp_follow():
     # 扫描二维码，关注公众号
     # img_url = 'https://login.weixin.qq.com/qrcode/gbXPB3iomg=='
-    scene = SCENE_PREFIX + random_string(5)
+    scene = 'mp-login-' + random_string(5)
     expire_time = 300
     data = {"expire_seconds": expire_time, "action_name": "QR_SCENE", "action_info": {"scene": {"scene_str": scene}}}
     try:
