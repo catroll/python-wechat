27f8df4c8c9e1d36e01240aa13a456ff5152b6ff
catroll <ninedoors@126.com>
2021-04-29 14:28:05 +0800

    Update example/views/__init__.py

--- example/views/__init__.py
+++ example/views/__init__.py
@@ -1,4 +1,20 @@
-from .auth import app as bp_auth  # noqa
-from .msg import app as bp_msg  # noqa
-from .pay import app as bp_pay  # noqa
-from .mp import app as bp_mp  # noqa
+import importlib
+import os
+
+from flask import Blueprint, current_app
+
+MODULES = []
+__all__ = 'MODULES', 'initialize'
+
+
+def initialize():
+    for i in os.listdir(os.path.dirname(__file__)):
+        if i.startswith('_') or not i.endswith('.py'):
+            continue
+        mod = importlib.import_module('views.' + i[:-3], 'views')
+        if mod in MODULES:
+            continue
+        MODULES.append(mod)
+        current_app.register_blueprint(getattr(mod, 'app'))
+        if hasattr(mod, 'initialize'):
+            getattr(mod, 'initialize')()
